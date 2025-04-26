a61c864efc6e615eb9277476dfe8f2615b306b45
Bnowako <blazejnowakowski98@gmail.com>
2025-04-26 13:47:08 +0200

    REMEMBER ME

--- twilio-bot/OpenAiAgentProcessor.py
+++ twilio-bot/OpenAiAgentProcessor.py
@@ -1,4 +1,4 @@
-from agents import Agent, Runner
+from agents import Agent, Runner, TResponseInputItem
 
 from pipecat.frames.frames import (
     Frame,
@@ -14,8 +14,9 @@ class OpenAiAgentProcessor(FrameProcessor):
     def __init__(self, participant_id: str):
         super().__init__()
         self._participant_id = participant_id
+        self.input_items: list[TResponseInputItem] = []
         self._agent = Agent(
-            name="Joker",
+            name="NFZ Assistant",
             instructions="You are a helpful assistant.",
         )
 
@@ -26,12 +27,15 @@ class OpenAiAgentProcessor(FrameProcessor):
             # Messages are accumulated on the context as a list of messages.
             # The last one by the human is the one we want to send to the LLM.
             text: str = frame.messages[-1]["content"]
-            result = Runner.run_streamed(self._agent, input=text.strip())
+            self.input_items.append({"content": text.strip(), "role": "user"})
+            result = Runner.run_streamed(self._agent, input=self.input_items)
             
             async for event in result.stream_events():
                 if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
                     print(event.data.delta, end="", flush=True)
                     await self.push_frame(TextFrame(event.data.delta))
+            
+            self.input_items = result.to_input_list()
 
 
         else:
